82ca875509843926495eef7516490235f6f700d9
Iqwertz <juliushussl@gmail.com>
2021-11-23 22:38:32 +0000

    updates?

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-let useBGApi: boolean = true; //used during dev. to limit api calls
+let useBGApi: boolean = false; //used during dev. to limit api calls
 const outputDir = `./bgremoved/`;
 let removedBgBase64: string = "";
 
@@ -53,7 +53,7 @@ let httpsServer: any;
 checkCertificate();
 
 const corsOptions = {
-  origin: "http://localhost:4200",
+  origin: "http://192.168.0.53",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
