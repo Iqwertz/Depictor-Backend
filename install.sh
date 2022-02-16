11aef1b6ff3d9cb7d35f59fa0d5e7577e0f4207c
Julius <juliushussl@gmail.com>
2022-02-16 16:41:31 +0100

    added gcode-cli install

--- install.sh
+++ install.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-echo starting Depictor install
+echo -e "\x1B[96m starting Depictor install \x1B[0m"
 
-echo updating system
+echo -e "\x1B[96m updating system \x1B[0m"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-echo installing Apache server
+echo -e "\x1B[96m installing Apache server  \x1B[0m"
 sudo apt-get install apache2 -y
-echo downloading latest Depictor Frontend Build
+echo -e "\x1B[96m downloading latest Depictor Frontend Build \x1B[0m"
 sudo rm /var/www/html/index.html
 sudo wget "https://github.com/Iqwertz/Depictor/releases/latest/download/Depictor-Build.zip" -O "/var/www/html/latest.zip"
 sudo unzip /var/www/html/latest.zip -d /var/www/html/
 sudo rm /var/www/html/latest.zip
 
-echo installing XVFB
+echo -e "\x1B[96m installing XVFB \x1B[0m"
 sudo apt-get install xvfb libxrender1 libxtst6 libxi6 -y
-echo installing Java
+echo -e "\x1B[96m installing Java \x1B[0m"
 sudo apt-get install default-jre -y
 
-echo starting Depictor Backend install
+echo -e "\x1B[96m installing gcode-cli \x1B[0m"
+echo -e "\x1B[96m cloning gcode-cli \x1B[0m"
+git clone https://github.com/hzeller/gcode-cli.git
+echo -e "\x1B[96m building gcode-cli \x1B[0m"
+cd gcode-cli
+make
+sudo cp gcode-cli /usr/bin/
+cd
 
-echo installing node
+echo -e "\x1B[96m starting Depictor Backend install \x1B[0m"
+
+echo -e "\x1B[96m installing node \x1B[0m"
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt-get install -y nodejs
-echo installing git
+echo -e "\x1B[96m installing git \x1B[0m"
 sudo apt install git -y
 
-echo downloading latest Depictor Backend releases
+echo -e "\x1B[96m downloading latest Depictor Backend releases \x1B[0m"
 LOCATION=$(curl -s https://api.github.com/repos/Iqwertz/Depictor-Backend/releases/latest \
 | grep "tag_name" \
 | awk '{print "https://github.com/Iqwertz/Depictor-Backend/archive/" substr($2, 2, length($2)-3) ".zip"}') \
@@ -38,19 +47,21 @@ cd Depictor-Backend-*
 mv * ../
 cd ../
 rm -r Depictor-Backend-*
-echo installing Node modules
+echo -e "\x1B[96m installing Node modules \x1B[0m"
 npm i
 
-echo chmodExecutables
+echo -e "\x1B[96m chmodExecutables \x1B[0m"
 sudo chmod +x chmodScripts.sh
 sudo ./chmodScripts.sh
 
-echo installing PM2
+echo -e "\x1B[96m installing PM2 \x1B[0m"
 sudo npm install pm2 -g
-echo configuring PM2 startup
+echo -e "\x1B[96m configuring PM2 startup \x1B[0m"
 pm2 startup | awk '$1 ~ /^sudo/' | bash
-echo starting node server
+echo -e "\x1B[96m starting node server \x1B[0m"
 pm2 start "sudo npm run start"
 pm2 save
 
-echo finished install
+rm install.sh
+
+echo -e "\x1B[96m finished install \x1B[0m"
