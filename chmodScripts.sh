3e060c7f7779d35c5423f2f69809c24b315a9d24
Julius <juliushussl@gmail.com>
2022-02-17 01:36:57 +0100

    frontend update script

--- chmodScripts.sh
+++ chmodScripts.sh
@@ -2,6 +2,7 @@
 
 sudo chmod +x scripts/home.sh
 sudo chmod +x scripts/execTemp.sh
+sudo chmod +x scripts/frontend-update.sh
 sudo chmod +x scripts/launchGcodeCli.sh
 sudo chmod +x scripts/launchimage2gcode.sh
 sudo chmod +x data/image2gcode/linux/Drawbot_stripped
